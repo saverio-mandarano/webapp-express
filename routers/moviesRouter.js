ea92cdcbd444d84582c9e17a6d30b6b03cccee60
saverio mandarano <saverio.mandarano1@gmail.com>
2026-02-20 17:40:45 +0100

    Add routes for movies

---
+++ routers/moviesRouter.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+
+// import e destrutturazione di moviesController
+const moviesController = require("../controllers/moviesController");
+const { index, show, store, update, modify, destroy } = moviesController;
+
+// rotta di index
+router.get("/", index);
+
+// rotta di show
+router.get("/:id", show);
+
+// store: creazione nuovo post
+router.post("/", store);
+
+// update: modifica integrale del post
+router.put("/:id", update);
+
+// modify: modifica parziale del post
+router.patch("/:id", modify);
+
+// destroy: cancellazione del post
+router.delete("/:id", destroy);
+
+module.exports = router;
